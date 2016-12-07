3abe78bb030bcb11cc47086cd5b6c59ebb8c602f
Yuriy Chaban <Yuriy Chaban>
2016-12-07 20:46:31 +0200

    finish work

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// init and customize slider
+
+$(document).ready(function(){
+
+  $('.shopfloristqueen-slider').slick({
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 1000,
+    centerPadding: '20px',
+    initialSlide: 1,
+    arrows: true,
+    slidesToShow: 3,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        infinite: true,
+        initialSlide: 0,
+        centerMode: true,
+        centerPadding: '10px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        centerPadding: '0',
+        initialSlide: 1,
+        slidesToShow: 1
+      }
+    }
+    ],
+    nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+    prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+  });
+
+});
